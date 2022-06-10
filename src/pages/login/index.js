6d216a7c65a1c01c7fcb42a1ebd8d4cd14f71c82
Jonas da Cunha Madeira <jonascn.2021@gmail.com>
2022-06-10 00:13:49 -0300

    finalizar a função do login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,33 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import axios from 'axios'
+
 import'./index.scss';
 
 export default function index() {
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+
+    const navigate = useNavigate();
+
+    async function entraClick() {
+        try{
+         const r = await axios.post('http://localhost:5000/usuario/login', {
+            nome: nome,
+            senha: senha
+            });
+           
+                 navigate('/menu');
+        
+        }
+        catch (err){
+            if (err.response.status === 401) {
+                setErro(err.response.data.erro);
+            }
+        }
+    }
     
 return(
 
@@ -18,13 +45,14 @@ return(
 
         <h3 className="area-restrita"> Área Restrita <br/> Login </h3>
 
-        <input type="text" id="imputUsuario" placeholder="Usuário" required="required" autofocus="autofocus" className="form-control border-left-0"/>
+        <input type="text" id="imputUsuario" placeholder="Usuário" required="required" autofocus="autofocus" className="form-control border-left-0" value={nome} onChange={e => setNome(e.target.value) }/>
         <p className="esqueceu"> Esqueceu seu nome de usuário? </p>
 
-        <input type="password" id="imputSenha" placeholder="Senha" required="required" className="form-control border-left-0"/>
+        <input type="password" id="imputSenha" placeholder="Senha" required="required" className="form-control border-left-0" value={senha} onChange={e => setSenha(e.target.value) }/>
         <p className="esqueceu"> Esqueceu sua senha? </p>
 
-        <a href="../menu" className="button"> Entrar </a>
+        <a href="../menu" className="button" onClick={entraClick}> Entrar </a>
+
          
     </div>  
     
